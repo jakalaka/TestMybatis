6f5a7d986164f9dad171d1287e3ef3508bb03a25
jakalaka <jakalaka@aliyun.com>
2017-07-20 13:52:15 +0800

    success

--- src/main/java/Mapper/CCPSXMapper.java
+++ src/main/java/Mapper/CCPSXMapper.java
@@ -1,16 +1,29 @@
 package Mapper;
 
 import Model.CCPSX;
+import Model.CCPSXExample;
 import Model.CCPSXKey;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
 
 public interface CCPSXMapper {
+    int countByExample(CCPSXExample example);
+
+    int deleteByExample(CCPSXExample example);
+
     int deleteByPrimaryKey(CCPSXKey key);
 
     int insert(CCPSX record);
 
     int insertSelective(CCPSX record);
 
+    List<CCPSX> selectByExample(CCPSXExample example);
+
     CCPSX selectByPrimaryKey(CCPSXKey key);
+
+    int updateByExampleSelective(@Param("record") CCPSX record, @Param("example") CCPSXExample example);
+
+    int updateByExample(@Param("record") CCPSX record, @Param("example") CCPSXExample example);
 
     int updateByPrimaryKeySelective(CCPSX record);
 
